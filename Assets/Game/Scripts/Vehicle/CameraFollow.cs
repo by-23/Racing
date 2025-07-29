45dcd0497ecfa346bb998e45c5251adaab04fdee
by23 <73724646+by-23@users.noreply.github.com>
2025-07-29 22:40:07 +0300

    Фиксы у камеры

--- Assets/Game/Scripts/Vehicle/CameraFollow.cs
+++ Assets/Game/Scripts/Vehicle/CameraFollow.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,15 +6,8 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
     public Transform Target => target;
-    [SerializeField] private float distance = 5;
     [SerializeField] private Vector3 offset = new Vector3(0f, 0, 0);
-    [SerializeField] private float moveTime = 1f;
-    [SerializeField] private Ease easing;
-    [SerializeField] private bool isAutoRotate;
 
-    private Vector3 _lastStableDirection;
-    private float _rotationSmoothVelocity;
-    private const float RotationSmoothTime = 0.3f;
     private Quaternion savedRotation;
     private Vehicle _ownerVehicle;
 
@@ -70,68 +62,19 @@ public class CameraFollow : MonoBehaviour
         if (!target) return;
 
         UpdateCameraPosition();
-        HandleCameraRotation();
+        transform.rotation = Quaternion.Euler(90f, savedRotation.eulerAngles.y, savedRotation.eulerAngles.z);
+
     }
 
     private void UpdateCameraPosition()
     {
-        Vector3 basePosition = target.position + Vector3.up * distance;
+        Vector3 basePosition = target.position + Vector3.up;
         Vector3 finalPosition = basePosition + offset;
 
-        transform.position = Vector3.Lerp(
-            transform.position,
-            finalPosition,
-            Time.deltaTime * moveTime
-        );
+        transform.position = finalPosition;
     }
-
-    private void HandleCameraRotation()
-    {
-        if (!isAutoRotate)
-        {
-            transform.rotation = Quaternion.Euler(90f, savedRotation.eulerAngles.y, savedRotation.eulerAngles.z);
-            return;
-        }
-
-        Vector3 horizontalForward = GetStableHorizontalDirection();
-        Quaternion targetRotation = Quaternion.Euler(90f, 0, 0) *
-                                    Quaternion.LookRotation(horizontalForward);
-
-        transform.rotation = SmoothDampQuaternion(
-            transform.rotation,
-            targetRotation,
-            ref _rotationSmoothVelocity,
-            RotationSmoothTime
-        );
-    }
-
-    private Vector3 GetStableHorizontalDirection()
-    {
-        Vector3 rawDirection = Vector3.ProjectOnPlane(target.forward, Vector3.up);
-
-        if (rawDirection == Vector3.zero)
-            return _lastStableDirection;
-
-        _lastStableDirection = rawDirection.normalized;
-        return _lastStableDirection;
-    }
-
-    private Quaternion SmoothDampQuaternion(
-        Quaternion current,
-        Quaternion target,
-        ref float velocity,
-        float smoothTime)
-    {
-        Vector3 currentEuler = current.eulerAngles;
-        Vector3 targetEuler = target.eulerAngles;
-
-        return Quaternion.Euler(
-            Mathf.SmoothDampAngle(currentEuler.x, targetEuler.x, ref velocity, smoothTime),
-            Mathf.SmoothDampAngle(currentEuler.y, targetEuler.y, ref velocity, smoothTime),
-            Mathf.SmoothDampAngle(currentEuler.z, targetEuler.z, ref velocity, smoothTime)
-        );
-    }
-
+    
+    
     public void SetTarget(Transform newTarget)
     {
         if (transform.parent != null)
@@ -142,19 +85,11 @@ public class CameraFollow : MonoBehaviour
         target = newTarget;
 
         // Мгновенный телепорт к цели
-        Vector3 basePosition = newTarget.position + Vector3.up * distance;
+        Vector3 basePosition = newTarget.position + Vector3.up;
         transform.position = basePosition + offset;
 
-        // Мгновенное обновление поворота
-        if (isAutoRotate)
-        {
-            _lastStableDirection = Vector3.ProjectOnPlane(newTarget.forward, Vector3.up).normalized;
-            Quaternion targetRotation = Quaternion.Euler(90f, 0, 0) * Quaternion.LookRotation(_lastStableDirection);
-            transform.rotation = targetRotation;
-        }
-        else
-        {
-            transform.rotation = Quaternion.Euler(90f, savedRotation.eulerAngles.y, savedRotation.eulerAngles.z);
-        }
+       
+        transform.rotation = Quaternion.Euler(90f, savedRotation.eulerAngles.y, savedRotation.eulerAngles.z);
+        
     }
 }
